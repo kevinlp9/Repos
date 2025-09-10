97bb63ba72ac4f41f070699b038f58e14c52ec60
kevinlp9 <atilano.kevin.h17@gmail.com>
2025-09-10 11:25:17 -0600

    10/9/25

--- src/Repo/Repositorio.java
+++ src/Repo/Repositorio.java
@@ -1,4 +1,17 @@
 package Repo;
 
-public class Repositorio {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Repositorio <T>{
+
+    private List<T> data = new ArrayList<>();
+
+    public void add(T obj){
+        data.add(obj);
+    }
+
+    public List<T> getAll(){
+        return data;
+    }
 }
